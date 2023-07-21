4e8d4fc9045e5afd76fee1dff0537640fb83f9fd
Ayesha Khan <ayeshaa.k3002@gmail.com>
2023-07-21 15:33:21 -0400

    Added comparison b/w time to run script

--- websiteBlocker.py
+++ websiteBlocker.py
@@ -7,6 +7,11 @@ redirect="127.0.0.1"
 # array of websites to block
 websiteList={"www.agar.io", "agar.io", "www.instagram.com", "instagram.com"}
 
+startTime = 8
+endTime = 16
+
 while True:
-    print(1)
+    # check 
+    if dt(dt.now().year, dt.now().month, dt.now().day, startTime) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, endTime):
+        print("Working hours")
     time.sleep(5)
